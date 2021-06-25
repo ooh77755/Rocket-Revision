ee8fac03a6d6fdb59456cde51bed89604af05b01
Alan Stewart <alanstewart07@hotmail.com>
2021-06-25 10:35:10 +0100

    Added 'movement' script and debug input for thrust and rotation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotate();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed space");
+        }
+    }
+
+    void ProcessRotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotate left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotate right");
+        }
+    }
+}
